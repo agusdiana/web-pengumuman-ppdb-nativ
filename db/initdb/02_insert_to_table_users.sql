5b8e28767738763c98a89f491c24a176600f39e9
agus diana <agusdiana00@gmail.com>
2025-10-02 13:05:09 +0700

    insert by initdb

---
+++ db/initdb/02_insert_to_table_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, password, email,role)
+VALUES ('admin', 'Password09', 'admin@example.com','admin');
+
+INSERT INTO users (username, password, email,role)
+VALUES ('siswa', 'siswa', 'siswaexample.com','siswa');
+
